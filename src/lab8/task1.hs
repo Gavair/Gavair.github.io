b8df29481af1a8141a24e839918d5c48b63e16e9
Taras Semenchenko <semtaras20@gmail.com>
2021-10-23 04:54:43 +0300

    Correct lab 8

--- src/lab8/task1.hs
+++ src/lab8/task1.hs
@@ -1,6 +1,29 @@
--- Функція яка знаходить інтеграл в текстовій формі
-get_integral :: String -> String
-get_integral x = "∫(" ++ x ++ ")dx"
+import Text.Read
+import Text.Regex.Posix
+
+-- Функція, яка перевіряє, чи є рядок числом
+is_number :: String -> Bool
+is_number str
+   | (number == Nothing) = False
+   | otherwise = True
+   where number = readMaybe str :: Maybe Double
+
+-- Функція яка вирішує прості інтеграли
+solve_integral :: String -> String
+solve_integral x
+   | (x == "0") = "C"
+   | (is_number x) = x ++ "*x + C"
+   | (x == "sin(x)") = "-cos(x) + C"
+   | (x =~ "\\`sin\\([0-9]*x\\)\\'" :: Bool) = "-cos(" ++ number ++ "x) / " ++ number ++ " + C"
+   | (x == "cos(x)") = "sin(x) + C"
+   | (x =~ "\\`cos\\([0-9]*x\\)\\'" :: Bool) = "sin(" ++ number ++ "x) / " ++ number ++ " + C"
+   | (x == "exp(x)") = x ++ " + C"
+   | (x =~ "\\`exp\\([0-9]*x\\)\\'" :: Bool) = x ++ " / " ++ number ++ " + C"
+   | (x =~ "\\`x\\^[0-9]+\\'" :: Bool) = "( x^(" ++ (show $ (read $ number :: Integer) + 1) ++ ") ) / " ++ (show $ (read $ number :: Integer) + 1) ++ " + C"
+   | (x =~ "\\`[0-9]+\\^x\\'" :: Bool) = x ++ " / ln(" ++ number ++ ") + C"
+   | (x =~ "\\`[0-9]+/x\\'" :: Bool) = "ln|" ++ number ++ "| + C"
+   | otherwise = "∫(" ++ x ++ ")dx"
+   where number = x =~ "[0-9]+" :: String
 
 -- Головна функція, з якої починається програма
 main = do
@@ -8,15 +31,15 @@ main = do
    let a = "-1"
    let f = "x*exp(x)"
 
-   putStrLn "Вхідні параметри:"
+   -- putStrLn "Вхідні параметри:"
    putStrLn $ "a(x) = " ++ a
    putStrLn $ "f(x) = " ++ f
 
    -- знаходження функції u
-   let u = "exp(" ++ (get_integral a) ++ ")"
+   let u = "exp(" ++ (solve_integral a) ++ ")"
    putStrLn "Інтегруючий множник:"
-   putStrLn $ "u = " ++ u
+   putStrLn $ "u(x) = " ++ u
 
    -- виведення загального рішення диференціального рівняння
    putStrLn "Загальне рішення диференціального рівняння:"
-   putStrLn $ "y = ("  ++ get_integral (u ++ "*" ++ f) ++ " + C) / (" ++ u ++ ")"
+   putStrLn $ "y = ("  ++ solve_integral (u ++ "*" ++ f) ++ " + C) / (" ++ u ++ ")"
